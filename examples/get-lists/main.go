2c1d20d0b342008b86ae0e49c9d7db05c7046ee0
John Moore <johnkenneth.moore@gmail.com>
2021-08-30 10:04:02 -0700

    add examples for folders and lists

---
+++ examples/get-lists/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Guitarbum722/clickup-client-go"
+)
+
+func main() {
+	if os.Args[1] == "" || os.Args[2] == "" {
+		panic("missing api key or team id")
+	}
+	apiKey := os.Args[1]
+	folderID := os.Args[2]
+
+	client := clickup.NewClient(&clickup.ClientOpts{
+		APIToken:   apiKey,
+		HTTPClient: nil,
+	})
+
+	lists, err := client.GetLists(folderID, false)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, folder := range lists.Lists {
+		fmt.Printf("List ID: %s\nName: %s\n\n", folder.ID, folder.Name)
+	}
+
+	list, err := client.GetList(os.Args[3])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Single Folder\n\nSpace ID: %s\nName: %s\n", list.ID, list.Name)
+}
